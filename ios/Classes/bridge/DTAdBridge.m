73c6732e092b734c7552f4230c2f9b230e81cc60
linkailong <HoiLungLam@outlook.com>
2024-08-29 16:04:55 +0800

    Fix iOS API type issue

--- ios/Classes/bridge/DTAdBridge.m
+++ ios/Classes/bridge/DTAdBridge.m
@@ -69,28 +69,28 @@
     [DTAdReport reportLoadBegin:id type:dtType platform:dtPlatform seq:seq mediation:dtMediation mediationId:mediationId properties:properties];
 }
 
-- (void)reportLoadEndId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform duration:(nonnull NSNumber *)duration result:(nonnull NSNumber *)result seq:(nonnull NSString *)seq errorCode:(nonnull NSNumber *)errorCode errorMessage:(nonnull NSString *)errorMessage properties:(nonnull NSDictionary<NSString *,id> *)properties mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+- (void)reportLoadEndId:(NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform duration:(NSInteger)duration result:(BOOL)result seq:(NSString *)seq errorCode:(NSInteger)errorCode errorMessage:(NSString *)errorMessage properties:(NSDictionary<NSString *, id> *)properties mediation:(DTAdMediationDart)mediation mediationId:(NSString *)mediationId error:(FlutterError *_Nullable *_Nonnull)error {
     DTAdType dtType = [DTEnumHelper converAdType:type];
     DTAdPlatform dtPlatform = [DTEnumHelper convertPlatformType:platform];
     DTAdMediation dtMediation = [DTEnumHelper convertMediationType:mediation];
 
-    [DTAdReport reportLoadEnd:id type:dtType platform:dtPlatform duration:duration result:[result boolValue] seq:seq mediation:dtMediation mediationId:mediationId errorCode:errorCode.intValue errorMessage:errorMessage properties:properties];
+    [DTAdReport reportLoadEnd:id type:dtType platform:dtPlatform duration:@(duration) result:result seq:seq mediation:dtMediation mediationId:mediationId errorCode:errorCode errorMessage:errorMessage properties:properties];
 }
 
-- (void)reportPaidId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq value:(NSNumber *)value currency:(nonnull NSString *)currency precision:(nonnull NSString *)precision properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+- (void)reportPaidId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq value:(double)value currency:(nonnull NSString *)currency precision:(nonnull NSString *)precision properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
     DTAdType dtType = [DTEnumHelper converAdType:type];
     DTAdPlatform dtPlatform = [DTEnumHelper convertPlatformType:platform];
     DTAdMediation dtMediation = [DTEnumHelper convertMediationType:mediation];
 
-    [DTAdReport reportPaid:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId value:[value doubleValue] currency:currency precision:precision properties:properties entrance:entrance];
+    [DTAdReport reportPaid:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId value:value currency:currency precision:precision properties:properties entrance:entrance];
 }
 
-- (void)reportPaidWithCountryId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId value:( NSNumber *)value precision:(nonnull NSString *)precision country:(nonnull NSString *)country properties:(nonnull NSDictionary<NSString *,id> *)properties error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+- (void)reportPaidWithCountryId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId value:(double)value precision:(nonnull NSString *)precision country:(nonnull NSString *)country properties:(nonnull NSDictionary<NSString *,id> *)properties error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
     DTAdType dtType = [DTEnumHelper converAdType:type];
     DTAdPlatform dtPlatform = [DTEnumHelper convertPlatformType:platform];
     DTAdMediation dtMediation = [DTEnumHelper convertMediationType:mediation];
 
-    [DTAdReport reportPaid:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId value:[value doubleValue] precision:precision country:country properties:properties entrance:@""];
+    [DTAdReport reportPaid:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId value:value precision:precision country:country properties:properties entrance:@""];
 }
 
 - (void)reportRewardedId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
@@ -101,12 +101,12 @@
     [DTAdReport reportRewarded:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId properties:properties entrance:entrance];
 }
 
-- (void)reportShowFailedId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq errorCode:(nonnull NSNumber *)errorCode errorMessage:(nonnull NSString *)errorMessage properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
+- (void)reportShowFailedId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq errorCode:(NSInteger)errorCode errorMessage:(nonnull NSString *)errorMessage properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
     DTAdType dtType = [DTEnumHelper converAdType:type];
     DTAdPlatform dtPlatform = [DTEnumHelper convertPlatformType:platform];
     DTAdMediation dtMediation = [DTEnumHelper convertMediationType:mediation];
 
-    [DTAdReport reportAdShowFail:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId errorCode:errorCode.intValue errorMessage:errorMessage properties:properties entrance:entrance];
+    [DTAdReport reportAdShowFail:id type:dtType platform:dtPlatform location:location seq:seq mediation:dtMediation mediationId:mediationId errorCode:errorCode errorMessage:errorMessage properties:properties entrance:entrance];
 }
 
 - (void)reportShowId:(nonnull NSString *)id type:(DTAdTypeDart)type platform:(DTAdPlatformDart)platform location:(nonnull NSString *)location seq:(nonnull NSString *)seq properties:(nonnull NSDictionary<NSString *,id> *)properties entrance:(nonnull NSString *)entrance mediation:(DTAdMediationDart)mediation mediationId:(nonnull NSString *)mediationId error:(FlutterError * _Nullable __autoreleasing * _Nonnull)error {
